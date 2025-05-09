7742264d27d5f0bf2728d686b6bb8443fe49d780
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-05-09 11:15:42 +0200

    move files to stopit2

--- src/stopit2/__init__.py
+++ src/stopit2/__init__.py
@@ -9,6 +9,7 @@ Public resources from ``stopit``
 
 try:
     from importlib.metadata import version
+
     __version__ = version(__name__)
 except Exception:
     # pkg_resources is deprecated as of Python 3.12 and no longer available for
@@ -16,17 +17,19 @@ except Exception:
     try:
         import pkg_resources
     except Exception:
-        LOG.warning(
-            "Could not get the package version from importlib or pkg_resources")
-        __version__ = 'unknown'
+        LOG.warning("Could not get the package version from importlib or pkg_resources")
+        __version__ = "unknown"
     else:
         __version__ = pkg_resources.get_distribution(__name__).version
 
-from .utils import LOG, TimeoutException
-from .threadstop import ThreadingTimeout, async_raise, threading_timeoutable
 from .signalstop import SignalTimeout, signal_timeoutable
+from .threadstop import ThreadingTimeout, async_raise, threading_timeoutable
+from .utils import LOG, TimeoutException
 
 __all__ = (
-    'ThreadingTimeout', 'async_raise', 'threading_timeoutable',
-    'SignalTimeout', 'signal_timeoutable'
+    "ThreadingTimeout",
+    "async_raise",
+    "threading_timeoutable",
+    "SignalTimeout",
+    "signal_timeoutable",
 )
